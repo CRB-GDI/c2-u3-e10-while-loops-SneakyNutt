8c0a9ff6ffb1b0eec4bb8d1bebd6d0e63918fac4
Griffith(Jeremiah) <145460979+SneakyNutt@users.noreply.github.com>
2024-03-06 10:20:49 -0500

    BenchMark

---
+++ Exercise 10(frl)/Exercise 10(frl)/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Exercise_10_frl_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a menu of choices
+            Console.WriteLine("Hello User, Welcome to The Game. Insert an applicable # to navigate the menu");
+
+            Console.WriteLine("|----------------------|");
+            Console.WriteLine("1-New Game");
+            Console.WriteLine("2-Load Game");
+            Console.WriteLine("3-Options");
+            Console.WriteLine("4-Credits");
+            Console.WriteLine("Credits");
+            Console.WriteLine("|----------------------|");
+            int answer= int.Parse(Console.ReadLine());
+            while (answer <1 || answer > 4)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Invalid Answer, please enter 1-4");
+
+                Console.WriteLine("Hello User, Welcome to The Game. Insert an applicable # to navigate the menu");
+                
+                Console.WriteLine("|----------------------|");
+                Console.WriteLine("1-New Game");
+                Console.WriteLine("2-Load Game");
+                Console.WriteLine("3-Options");
+                Console.WriteLine("4-Credits");
+                Console.WriteLine("Credits");
+                Console.WriteLine("|----------------------|");
+                answer = int.Parse(Console.ReadLine());
+            }
+
+            //print response 
+            if (answer == 1)
+            {
+                Console.WriteLine("Starting a new game...");
+            }
+            if (answer == 2)
+            {
+                Console.WriteLine("Select a File to Load");
+                Console.WriteLine("1-File 1");
+                Console.WriteLine("1-File 2");
+                Console.WriteLine("1-File 3");
+
+                int.Parse(Console.ReadLine());
+                int answer2 = int.Parse(Console.ReadLine());
+                {
+                    if (answer2 == 1)
+                    {
+                        Console.WriteLine("Loading...");
+                    }
+                }
+               
+            }
+        }
+    }
+}
