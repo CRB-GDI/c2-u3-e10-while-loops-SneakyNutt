ab5a5aeee42006ffe4705c8b5693dc6aa5299d0f
Griffith(Jeremiah) <145460979+SneakyNutt@users.noreply.github.com>
2024-03-13 09:21:05 -0400

    Done I think...(time to refactor)

--- Exercise 10(frl)/Exercise 10(frl)/Program.cs
+++ Exercise 10(frl)/Exercise 10(frl)/Program.cs
@@ -12,27 +12,27 @@
             Console.WriteLine("2-Load Game");
             Console.WriteLine("3-Options");
             Console.WriteLine("4-Credits");
-            Console.WriteLine("Credits");
+            Console.WriteLine("5-Exit");
             Console.WriteLine("|----------------------|");
-            int answer= int.Parse(Console.ReadLine());
-            while (answer <1 || answer > 4)
+            int answer = int.Parse(Console.ReadLine());
+            while (answer < 1 || answer > 6)
             {
                 Console.WriteLine();
-                Console.WriteLine("Invalid Answer, please enter 1-4");
+                Console.WriteLine("Invalid Answer, please enter 1-5");
 
                 Console.WriteLine("Hello User, Welcome to The Game. Insert an applicable # to navigate the menu");
-                
+
                 Console.WriteLine("|----------------------|");
                 Console.WriteLine("1-New Game");
                 Console.WriteLine("2-Load Game");
                 Console.WriteLine("3-Options");
                 Console.WriteLine("4-Credits");
-                Console.WriteLine("Credits");
+                Console.WriteLine("5-Exit");
                 Console.WriteLine("|----------------------|");
                 answer = int.Parse(Console.ReadLine());
             }
 
-            //print response 
+            //print response 1
             if (answer == 1)
             {
                 Console.WriteLine("Starting a new game...");
@@ -41,18 +41,39 @@
             {
                 Console.WriteLine("Select a File to Load");
                 Console.WriteLine("1-File 1");
-                Console.WriteLine("1-File 2");
-                Console.WriteLine("1-File 3");
+                Console.WriteLine("2-File 2");
+                Console.WriteLine("3-File 3");
 
-                int.Parse(Console.ReadLine());
+                //print response 2
                 int answer2 = int.Parse(Console.ReadLine());
                 {
-                    if (answer2 == 1)
+                    if (answer2 == 1 || answer2 == 2 || answer2 == 3)
                     {
                         Console.WriteLine("Loading...");
+
                     }
                 }
-               
+            }
+            //print response 3
+            if (answer == 3)
+            {
+                Console.WriteLine("SFX");
+                Console.WriteLine("Voices");
+                Console.WriteLine("Language");
+                Console.WriteLine("Finished");
+            }
+            {
+                //print response 4
+                if (answer == 4)
+                {
+                    Console.WriteLine("Shoutout to Christian, Justin, Micah. Ms. Gallant, Mari, Ari, Bri, Keegan, Trevor, Lyric, Layla, Ben, and lastly the Pikmin gang. Luv u all.");
+                }
+
+            }
+            //print response 5
+            if (answer == 5)
+            {
+                Console.WriteLine("Thank You for Playing :)");
             }
         }
     }
